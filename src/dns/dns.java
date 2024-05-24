c3b29716773f01314507034172e717f4e91b2fc0
katharine <msksmny@gmail.com>
2024-05-24 14:42:00 -0400

    moved dns subdirectory back into src

---
+++ src/dns/dns.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.*;
+
+public class dns {
+    private final ExecutorService executor = Executors.newFixedThreadPool(10); 
+    private final Options globalopts; //tbd
+    private final OptionsDNS options;
+    private final boolean isWildcard = false;
+    private final Set<InetAddress> wildcardIps = new HashSet<>();
+    private final String resolver;
+   
+   public dns(Options globalopts, OptionsDNS options) throws Exception {
+        if (globalopts == null) {
+            throw new IllegalArgumentException("please provide valid global options");
+        }
+
+        if (options == null) {
+            throw new IllegalArgumentException("please provide valid plugin options");
+        }
+
+        this.globalopts = globalopts;
+        this.options = options;
+
+        return dns;
+    }
+}
+
